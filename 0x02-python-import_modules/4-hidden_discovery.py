611c45a9b9d6a5e96c1b3c1d83d8a24e41b4be6d
Matt <lxpirez133@gmail.com>
2020-12-10 14:21:56 -0300

    push

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-if __name__ == "__main__"
-for i in range(len(dir(hidden_4))):
-    if dir(hidden_4)[i][:2] != '__':
-        print("{}".format(dir(hidden_4)[i]))   
+if __name__ == "__main__":
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][:2] != '__':
+            print("{}".format(dir(hidden_4)[i]))   
