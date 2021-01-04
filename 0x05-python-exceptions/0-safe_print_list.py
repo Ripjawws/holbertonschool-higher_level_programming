659d726bade5b0f4abd443e00c7905998ffc7ae2
Matias <lxpirez133@gmail.com>
2021-01-04 16:18:56 +0000

    exercises

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    new_str = ""
+    for i in my_list[:x]:
+        new_str += str(i)
+        j += 1
+    try:
+        print(new_str)
+        return j
+    except:
+        print(Error)
