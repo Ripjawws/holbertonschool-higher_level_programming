b1f7b20ff77e63d04f98b1727e3fa62223fab53a
Matias <lxpirez133@gmail.com>
2021-01-05 23:43:25 +0000

    exercises

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Create a class"""
+    def __init__(self, size="0"):
+        """Initializing data"""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Create a class"""
+    def __init__(self, size=0):
+        """Initializing data"""
+        self.__size = size
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
