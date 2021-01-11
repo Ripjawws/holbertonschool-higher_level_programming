b1f68b0e4ab877ed503d9a3838c07c853c4f7e87
Matias <lxpirez133@gmail.com>
2021-01-11 23:05:33 +0000

    code

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle:
     Create an empty rectangle class
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
