e53c6f7db9e72d2c44b362b302a785d656e04864
Matt <lxpirez133@gmail.com>
2021-03-22 18:52:37 +0000

    exercises 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,9 +4,8 @@ Prints if its number
 */
 let num = 0;
 num = parseInt(process.argv[2]);
-if(num) {
-    console.log('My number: ' + num);
-} 
-else {
-    console.log('Not a number')
+if (num) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
 }
