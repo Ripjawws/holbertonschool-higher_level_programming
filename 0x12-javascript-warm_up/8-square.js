bbc9a892f0519ca406449ff9bedf239b68c53b8b
Matt <lxpirez133@gmail.com>
2021-03-22 20:00:27 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,10 @@
 Script that prints a square
 */
 const a = parseInt(process.argv[2]);
-for (let i = 0; i < a; i++) {
-  console.log('x'.repeat(a));
-}
 if (isNaN(a) === true) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < a; i++) {
+    console.log('x'.repeat(a));
+  }
 }
