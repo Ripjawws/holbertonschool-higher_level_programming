607f1eefc883913fea9e8cc551bf25dc69021cc8
Matt <lxpirez133@gmail.com>
2021-03-22 18:49:30 +0000

    first exercises

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/*
+Concats "is"
+*/
+console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Prints if its number
+*/
+let num = 0;
+num = parseInt(process.argv[2]);
+if(num) {
+    console.log('My number: ' + num);
+} 
+else {
+    console.log('Not a number')
+}
