13ecac72e87321b310062a468891b50fd687a1ec
Matt <lxpirez133@gmail.com>
2021-03-27 14:31:42 +0000

    exercise

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name like 'N%'\
+    cur.execute("SELECT * FROM states WHERE name like BINARY 'N%'\
          ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """script that takes in an argument and displays all
 values in the states table of hbtn_0e_0_usa where name matches the argument."""
-if __name__ == "__main__":
-    import MySQLdb
+"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+if __name__ == '__main__':
     from sys import argv
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-        like %s ORDER BY id ASC", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE " +
+                "BINARY '{}'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-      print("{}".format(row))
-    # Close all cursors
+        print("{}".format(row))
     cur.close()
-    # Close all databases
     db.close()
